fa3ecf95ce87219d3c7d50b27e9f99533f359cf1
imamsalji <imamsalji7@gmail.com>
2026-02-26 16:09:59 +0700

    update admin api

---
+++ src/api/admin.ts
@@ -0,0 +1,102 @@
+import api from "./axios"; // axios instance kamu
+import type { Game } from "../types/Game";
+import type { Product } from "../types/Product";
+import type {
+  OrderItem,
+  GetOrders,
+  CheckoutPayload,
+  CheckoutResponse,
+  UploadPaymentResponse,
+} from './../types/Order'
+
+/* =========================
+   DASHBOARD
+========================= */
+
+export const getAdminDashboard = async () => {
+  const response = await api.get("/admin/dashboard");
+  return response.data;
+};
+
+/* =========================
+   GAME MANAGEMENT
+========================= */
+
+export const getAllGames = async () => {
+  const response = await api.get("/admin/games");
+  return response.data.data;
+};
+
+export const createGame = async (payload: FormData) => {
+  const response = await api.post("/admin/games", payload, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const updateGame = async (id: number, payload: FormData) => {
+  const response = await api.put(`/admin/games/${id}`, payload, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const deleteGame = async (id: number) => {
+  const response = await api.delete(`/admin/games/${id}`);
+  return response.data;
+};
+
+/* =========================
+   PRODUCT MANAGEMENT
+========================= */
+
+export const getAllProducts = async (): Promise<Product[]> => {
+  const response = await api.get("/admin/products");
+  return response.data.data;
+};
+
+export const createProduct = async (payload: FormData) => {
+  const response = await api.post("/admin/products", payload, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const updateProduct = async (id: number, payload: FormData) => {
+  const response = await api.put(`/admin/products/${id}`, payload, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const deleteProduct = async (id: number) => {
+  const response = await api.delete(`/admin/products/${id}`);
+  return response.data;
+};
+
+/* =========================
+   ORDER MANAGEMENT
+========================= */
+
+export const getAllOrders = async (): Promise<OrderItem[]> => {
+  const response = await api.get("/admin/orders");
+  return response.data.data;
+};
+
+export const verifyOrder = async (id: number) => {
+  const response = await api.patch(`/admin/orders/${id}/verify`);
+  return response.data;
+};
+
+export const rejectOrder = async (id: number) => {
+  const response = await api.patch(`/admin/orders/${id}/reject`);
+  return response.data;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import App from "./App";
 import { AppWrapper } from "./component/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import "./assets/css/index.css";
-
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 AOS.init({
   duration: 800,
   easing: "ease-in-out",
@@ -16,8 +17,10 @@ AOS.init({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <AppWrapper>
-      <App />
-    </AppWrapper>
+    <QueryClientProvider client={queryClient}>
+      <AppWrapper>
+        <App />
+      </AppWrapper>
+    </QueryClientProvider>
   </ThemeProvider>,
 );
