e201cf874c2e7c16ffde171888b820f22899e077
imamsalji <imamsalji7@gmail.com>
2026-02-27 22:08:24 +0700

    update image

--- src/pages/AllGame.tsx
+++ src/pages/AllGame.tsx
@@ -1,37 +1,41 @@
 import { useEffect, useState } from "react";
-import {getGames} from "../api/game";
-import type {Game} from "../types/Game";
-import logo from './../assets/img/logoMosher.jpeg';
+import { getGames } from "../api/game";
+import type { Game } from "../types/Game";
+import logo from "./../assets/img/logoMosher.jpeg";
 import CyberpunkCard from "../component/transaksi/CyberpunkCard";
 import CyberpunkSpinner from "../component/transaksi/CyberpunkSpinner";
 
 export default function AllGame() {
-    const [game, setGame] = useState<Game[]>([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const fetchGame = async () => {
-            try {
-                const data = await getGames()
-                setGame(data)
-            } catch (error) {
-                console.error(error)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchGame()
-    }, []);
-    console.log(game);
-    
+  const [game, setGame] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchGame = async () => {
+      try {
+        const data = await getGames();
+        setGame(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGame();
+  }, []);
+  console.log(game);
 
-    return (
-    <section id="games" className="container mx-auto px-6 md:px-10 py-16 text-center">
-        <h2 className="text-2xl md:text-3xl font-['Comic_Sans_MS',_cursive] font-bold mb-10">Games</h2>
-            {game.length === 0 ? (
-                <CyberpunkSpinner size={80} text="Loading" />
-            ) : (
-            <div
-                className="
+  return (
+    <section
+      id="games"
+      className="container mx-auto px-6 md:px-10 py-16 text-center"
+    >
+      <h2 className="text-2xl md:text-3xl font-['Comic_Sans_MS',_cursive] font-bold mb-10">
+        Games
+      </h2>
+      {game.length === 0 ? (
+        <CyberpunkSpinner size={80} text="Loading" />
+      ) : (
+        <div
+          className="
                     grid
                     grid-cols-2
                     sm:grid-cols-3
@@ -39,16 +43,16 @@ export default function AllGame() {
                     lg:grid-cols-6
                     gap-6
                 "
-                >
-                {game.map((item) => (
-                <CyberpunkCard
-                    title={item.name}
-                    image={logo}
-                    tag={item.slug}
-                />
-                ))}
-            </div>
-            )}
+        >
+          {game.map((item) => (
+            <CyberpunkCard
+              title={item.name}
+              image={item.image_url}
+              tag={item.slug}
+            />
+          ))}
+        </div>
+      )}
     </section>
-    );
+  );
 }
