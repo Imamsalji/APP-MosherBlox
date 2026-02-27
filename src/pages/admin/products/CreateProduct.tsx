988548a414575131c5adacca9862905502314bdf
imamsalji <imamsalji7@gmail.com>
2026-02-27 22:14:48 +0700

    Update create Product

--- src/pages/admin/products/CreateProduct.tsx
+++ src/pages/admin/products/CreateProduct.tsx
@@ -1,28 +1,101 @@
 import PageBreadcrumb from "../../../component/common/PageBreadCrumb";
-import DefaultInputs from "../../../component/form/form-elements/DefaultInputs";
-import InputGroup from "../../../component/form/form-elements/InputGroup";
-import CheckboxComponents from "../../../component/form/form-elements/CheckboxComponents";
-import FileInputExample from "../../../component/form/form-elements/FileInputExample";
+import { useState } from "react";
+import ComponentCard from "../../../component/common/ComponentCard";
+import Label from "../../../component/form/Label.tsx";
+import Input from "../../../component/form/input/InputField";
 import PageMeta from "../../../component/common/PageMeta";
+import FileInput from "../../../component/form/input/FileInput";
+import TextArea from "../../../component/form/input/TextArea";
+import Radio from "../../../component/form/input/Radio";
 
 export default function CreateProduct() {
+  const [message, setMessage] = useState("");
+  const [selectedValue, setSelectedValue] = useState<string>("option2");
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+  };
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      console.log("Selected file:", file.name);
+    }
+  };
   return (
     <div>
       <PageMeta
-        title="React.js Form Elements Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        title=" Detail Game"
         description="This is React.js Form Elements  Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="Form Elements" />
-      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
+      <PageBreadcrumb pageTitle="Game" />
+      <ComponentCard title="Tambah Detail Game">
         <div className="space-y-6">
-          <DefaultInputs />
+          <div>
+            <Label htmlFor="name">name</Label>
+            <Input
+              type="text"
+              id="input"
+              error
+              hint={"This is an invalid email address."}
+            />
+          </div>
+          <div>
+            <Label htmlFor="slug">slug</Label>
+            <Input
+              type="text"
+              id="slug"
+              error
+              hint={"This is an invalid email address."}
+            />
+          </div>
+          <div>
+            <Label>Upload Image</Label>
+            <FileInput onChange={handleFileChange} className="custom-class" />
+          </div>
+          <div className="space-y-6">
+            {/* Default TextArea */}
+            <div>
+              <Label>Description</Label>
+              <TextArea
+                value={message}
+                onChange={(value) => setMessage(value)}
+                rows={6}
+                // hint="Please enter a valid message."
+              />
+            </div>
+            {/* Error TextArea */}
+            {/* <div>
+              <Label>Description</Label>
+              <TextArea
+                rows={6}
+                value={messageTwo}
+                error
+                onChange={(value) => setMessageTwo(value)}
+                hint="Please enter a valid message."
+              />
+            </div> */}
+          </div>
+          <div className="flex flex-wrap items-center gap-8">
+            <Label>Status Game</Label>
+            <Radio
+              id="radio1"
+              name="group1"
+              value="option1"
+              checked={selectedValue === "option1"}
+              onChange={handleRadioChange}
+              label="Aktif"
+            />
+            <Radio
+              id="radio2"
+              name="group1"
+              value="option2"
+              checked={selectedValue === "option2"}
+              onChange={handleRadioChange}
+              label="Non-Aktif"
+            />
+          </div>
         </div>
-        <div className="space-y-6">
-          <InputGroup />
-          <FileInputExample />
-          <CheckboxComponents />
-        </div>
-      </div>
+      </ComponentCard>
     </div>
   );
 }
