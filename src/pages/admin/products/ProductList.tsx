b1251ff2cad2311db65e18b783e4a08f9504b98c
imamsalji <imamsalji7@gmail.com>
2026-02-27 15:34:28 +0700

    list Product Update

--- src/pages/admin/products/ProductList.tsx
+++ src/pages/admin/products/ProductList.tsx
@@ -1,9 +1,35 @@
 import PageBreadcrumb from "../../../component/common/PageBreadCrumb";
 import ComponentCard from "../../../component/common/ComponentCard";
 import PageMeta from "../../../component/common/PageMeta";
-import BasicTableOne from "../../../component/tables/BasicTables/BasicTableOne";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAllProducts, deleteProduct } from "./../../../api/admin";
+import Button from "../../../component/ui/button/Button";
+import type { Product } from "./../../../types/Product";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../../../component/ui/table";
+import Badge from "../../../component/ui/badge/Badge";
 
 export default function ProductList() {
+  const queryClient = useQueryClient();
+
+  const { data: product, isLoading } = useQuery({
+    queryKey: ["admin-games"],
+    queryFn: getAllProducts,
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["admin-games"] });
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <>
       <PageMeta
@@ -13,7 +39,101 @@ export default function ProductList() {
       <PageBreadcrumb pageTitle="Game Detail" />
       <div className="space-y-6">
         <ComponentCard title="List Game">
-          <BasicTableOne />
+          <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+            <div className="max-w-full overflow-x-auto">
+              <Table>
+                {/* Table Header */}
+                <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+                  <TableRow>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      Image
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      Name
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      Harga
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      stock
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      status
+                    </TableCell>
+                    <TableCell
+                      isHeader
+                      className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                    >
+                      aksi
+                    </TableCell>
+                  </TableRow>
+                </TableHeader>
+
+                {/* Table Body */}
+                <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+                  {product?.map((order: Product) => (
+                    <TableRow key={order.id}>
+                      <TableCell className="px-5 py-4 sm:px-6 text-start">
+                        <div className="flex items-center gap-3">
+                          <div className="w-10 h-10 overflow-hidden rounded-full">
+                            <img
+                              width={40}
+                              height={40}
+                              src={order.image_url}
+                              alt={order.name}
+                            />
+                          </div>
+                        </div>
+                      </TableCell>
+                      <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                        {order.name}
+                      </TableCell>
+                      <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                        {order.price}
+                      </TableCell>
+                      <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                        {order.stock}
+                      </TableCell>
+                      <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                        <Badge
+                          size="sm"
+                          color={order.status === 1 ? "success" : "error"}
+                        >
+                          {order.status === 1 ? "Aktif" : "Non-Aktif"}
+                        </Badge>
+                      </TableCell>
+
+                      <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                        <div className="flex items-center gap-5">
+                          <Button size="sm" variant="primary">
+                            Edit
+                          </Button>
+                          <Button size="sm" variant="danger">
+                            Delete
+                          </Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </div>
         </ComponentCard>
       </div>
     </>
