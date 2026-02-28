f8fc5f4065634dbd1e4cbc8f4e04e638c93a7ed5
imamsalji <imamsalji7@gmail.com>
2026-02-28 22:21:02 +0700

    type gamelist

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -12,3 +12,14 @@ export interface Game {
   created_at?: string
 }
 
+export interface GameList {
+  id: number
+  name: string
+  slug: string
+  image: File | null
+  image_url: string
+  description:string
+  status: number | string
+  created_at: string
+}
+
