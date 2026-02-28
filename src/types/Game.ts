dc344abf5358a7f67f2f9cf7eed2fa450c438fc4
imamsalji <imamsalji7@gmail.com>
2026-02-28 17:54:23 +0700

    update type

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -2,13 +2,13 @@
  * GAME TYPE
  */
 export interface Game {
-  id: number
+  id?: number
   name: string
   slug: string
-  image: string
-  image_url: string
+  image: File | null
+  image_url?: string
   description:string
-  status: number
-  created_at: string
+  status: number | string
+  created_at?: string
 }
 
