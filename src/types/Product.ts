635e50d21ac811833a5da668ca0f32dca8540051
imamsalji <imamsalji7@gmail.com>
2026-03-01 22:13:50 +0700

    type product

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,5 +1,23 @@
 import type { Game } from './Game'
 export interface Product {
+  id?: number
+  game_id: number 
+
+  name: string
+  price: number
+  specification: string
+
+  image: File | null
+  image_url?: string
+  stock: number
+  status: number
+
+  created_at?: string
+  updated_at?: string
+  game?:Game
+}
+
+export interface ProductList {
   id: number
   game_id: number
 
